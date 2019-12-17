51c65c8e560db3e91bf9c452be67cae1603806f2
ade-clim <a.declimmer@gmail.com>
2019-12-17 19:17:35 +0100

    ajout event pour lier automatiquement la creation d'une commande à l'utilisateur connecté

--- src/Entity/Orders.php
+++ src/Entity/Orders.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\OrdersRepository")
@@ -39,6 +40,7 @@ class Orders
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="orders")
      * @Groups({"order_read"})
+     * @Assert\NotBlank(message="L'utilisateur est obligatoire")
      */
     private $user;
 
